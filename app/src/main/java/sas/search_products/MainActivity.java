fd810a8aff9428051af02fc0e1fa1b584497bcb3
PacoLapLinux <paco.morando@gmail.com>
2023-11-27 11:16:58 -0600

    Init commit

---
+++ app/src/main/java/sas/search_products/MainActivity.java
@@ -0,0 +1,14 @@
+package sas.search_products;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
